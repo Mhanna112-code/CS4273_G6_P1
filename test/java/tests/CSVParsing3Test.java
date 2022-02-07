4d5e7b864a2c45a1e0adbe5ec98bd5a8641d2255
Mhanna112-code <60158039+Mhanna112-code@users.noreply.github.com>
2022-02-06 22:52:21 -0600

    Update CSVParsing3Test.java

--- test/java/tests/CSVParsing3Test.java
+++ test/java/tests/CSVParsing3Test.java
@@ -1,5 +1,7 @@
-import main.java.CSVParsing.java
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 class CSVParsingTest3 {
 
 	
